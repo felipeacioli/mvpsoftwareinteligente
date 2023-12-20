692d5a155bed6942bd7332851a48b5ef88dba5e0
felipeacioli <105074353+felipeacioli@users.noreply.github.com>
2023-12-20 06:38:32 -0300

    Update test_modelos.py

--- api/test_modelos.py
+++ api/test_modelos.py
@@ -20,23 +20,7 @@ dataset = carregador.carregar_dados(url_dados, colunas)
 X = dataset.iloc[:, 0:-1]
 Y = dataset.iloc[:, -1]
     
-# Método para testar o modelo de Regressão Logística a partir do arquivo correspondente
-# O nome do método a ser testado necessita começar com "test_"
-def test_modelo_lr():  
-    # Importando o modelo de regressão logística
-    lr_path = 'ml_model/diabetes_lr.pkl'
-    modelo_lr = modelo.carrega_modelo(lr_path)
 
-    # Obtendo as métricas da Regressão Logística
-    acuracia_lr, recall_lr, precisao_lr, f1_lr = avaliador.avaliar(modelo_lr, X, Y)
-    
-    # Testando as métricas da Regressão Logística 
-    # Modifique as métricas de acordo com seus requisitos
-    assert acuracia_lr >= 0.75 
-    assert recall_lr >= 0.5 
-    assert precisao_lr >= 0.5 
-    assert f1_lr >= 0.5 
- 
 # Método para testar modelo KNN a partir do arquivo correspondente
 def test_modelo_knn():
     # Importando modelo de KNN
@@ -47,10 +31,10 @@ def test_modelo_knn():
     acuracia_knn, recall_knn, precisao_knn, f1_knn = avaliador.avaliar(modelo_knn, X, Y)
     
     # Testando as métricas do KNN
-    # Modifique as métricas de acordo com seus requisitos
-    assert acuracia_knn >= 0.75
-    assert recall_knn >= 0.5 
-    assert precisao_knn >= 0.5 
-    assert f1_knn >= 0.5 
+    
+    assert acuracia_knn >= 0.70
+    assert recall_knn >= 0.52 
+    assert precisao_knn >= 0.52 
+    assert f1_knn >= 0.58 
     
 
