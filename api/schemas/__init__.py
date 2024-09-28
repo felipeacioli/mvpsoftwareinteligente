744b33cc44d112570b24d6b3092e13525dcdb925
felipeacioli <105074353+felipeacioli@users.noreply.github.com>
2024-09-27 23:47:13 -0300

    Delete api directory

--- api/schemas/__init__.py
+++
@@ -1,6 +0,0 @@
-from schemas.amostra_schema import AmostraSchema,  AmostraBuscaSchema, AmostraViewSchema, AmostraDelSchema, \
-                                    apresenta_amostras, apresenta_amostra
-                                        
-from schemas.error_schema import ErrorSchema
-                                    
-                                    
